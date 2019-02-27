1fe48846ce6e307615b84dfd60dde9e5314b767b
Rob Superty <ytrepus@yahoo.com>
2019-02-27 09:48:23 +0000

    Check for CASE_INSENSITIVE env variable.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
@@ -15,7 +16,9 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
