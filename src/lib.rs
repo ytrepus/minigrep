bdcdf889e9a41f4bc395c3299690875f03c18c0c
Rob Superty <ytrepus@yahoo.com>
2019-02-27 09:42:37 +0000

    Implemented case insensitive query.

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,19 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
