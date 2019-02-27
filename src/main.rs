cdde220c8fa6bcb949a2c8443cbb47fb7f0c4f4a
Rob Superty <ytrepus@yahoo.com>
2019-02-27 00:19:16 +0000

    Added lib imports into main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 use std::process;
 
+use minigrep;
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -12,7 +15,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
