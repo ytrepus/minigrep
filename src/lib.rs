03a8810a456611ac79fdf50daf952568cf212162
Rob Superty <ytrepus@yahoo.com>
2019-02-27 09:30:03 +0000

    Implemented search

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
